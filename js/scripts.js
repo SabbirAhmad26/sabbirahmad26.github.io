625c7654c452d94671f8370c9c2d52d16947a9fb
sabbir <97406793+SabbirAhmad26@users.noreply.github.com>
2024-11-11 02:37:08 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
+// script.js
+function toggleAbstract(id) {
+    var element = document.getElementById(id);
+    element.style.display = (element.style.display === 'none') ? 'block' : 'none';
+}
+
+function toggleBibtex(id) {
+    var element = document.getElementById(id);
+    element.style.display = (element.style.display === 'none') ? 'block' : 'none';
+}
 
